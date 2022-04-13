6ab18ed4e018a5dfe367dc1957decc6f52a9dd3c
goyalkaran <goyalkaran2001@gmail.com>
2022-04-14 03:54:14 +0530

    10th_commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:shoppers_paradise/util/routes.dart';
 import 'package:shoppers_paradise/view/homepage.dart';
 import 'package:shoppers_paradise/view/loginpage.dart';
 import 'package:shoppers_paradise/widgets/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,10 +22,50 @@ class MyApp extends StatelessWidget {
       darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
-        "/": (context) => LoginPage(),
+        "/": (context) => FutureBuilder(
+              future: _firebaseApp,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  print('error hai!! ${snapshot.error.toString()}');
+                  return Text('kuch toh gadbad h');
+                } else if (snapshot.hasData) {
+                  return LoginPage();
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
         MyRoutes.homeRoute: (context) => HomePage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
       },
+      // home: FutureBuilder(
+      //   future: _firebaseApp,
+      //   builder: (context, snapshot) {
+      //     if (snapshot.hasError) {
+      //       print('error hai!! ${snapshot.error.toString()}');
+      //       return Text('kuch toh gadbad h');
+      //     } else if (snapshot.hasData) {
+      //       return (routes: {
+      //
+      //     "/": (context) => LoginPage(),
+      //     MyRoutes.homeRoute: (context) => HomePage(),
+      //     MyRoutes.loginRoute: (context) => LoginPage(),
+      //     });
+      //     } else {
+      //       return Center(
+      //         child: CircularProgressIndicator(),
+      //       );
+      //     }
+      //   },
+      // ),
+      // routes: {
+      //
+      //   "/": (context) => LoginPage(),
+      //   MyRoutes.homeRoute: (context) => HomePage(),
+      //   MyRoutes.loginRoute: (context) => LoginPage(),
+      // },
     );
   }
 }
